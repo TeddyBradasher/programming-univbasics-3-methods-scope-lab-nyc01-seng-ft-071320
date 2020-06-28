1aa09ec0134558b576e1dd58ab865db81742a9e0
Teddy Bradsher <teddybradsher@gmail.com>
2020-06-28 12:22:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,3 +6,16 @@ end
 def toadstool
   puts status
 end
+describe "mario" do
+  it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
+end
+describe "all_phrases" do
+  it "puts out all of the previous catch phrases" do
+    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
+    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
+    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
+  end
+end
